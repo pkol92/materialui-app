6bac10ce017612894422e21eca08fda272b9139d
Paulina <paulina.kolasa@espeo.eu>
2023-04-17 14:05:36 +0200

    add back btn

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <Link href="/form">
-          <Button>Klik</Button>
+          <Button>Register</Button>
         </Link>
       </div>
     </main>
