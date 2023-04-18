8dc02fb0338958240322f3084a310f5cbcd0589b
Paulina <paulina.kolasa@espeo.eu>
2023-04-18 16:43:49 +0200

    add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,18 @@ import Link from "next/link";
 import Button from "@mui/material/Button";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { push } = useRouter();
-  useEffect(() => {
-    push("/login");
-  }, [push]);
+  // const { push } = useRouter();
+  // useEffect(() => {
+  //   push("/login");
+  // }, [push]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+    <main>
+      <Navbar />
     </main>
   );
 }
