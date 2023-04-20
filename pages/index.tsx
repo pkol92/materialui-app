60512a22470a2024f3286534c141673d7a223e97
Paulina <paulina.kolasa@espeo.eu>
2023-04-20 13:55:11 +0200

    change menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar";
 import { EnhancedTable } from "@/components/table";
+import { ResponsiveNavbar } from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <Navbar />
-      <div className="m-auto pt-20 pb-10 pr-4 ps-4 max-w-screen-md">
+      <ResponsiveNavbar />
+      <div className="m-auto pt-20 pb-10 pr-2 ps-2 max-w-screen-md">
         <EnhancedTable />
       </div>
     </main>
